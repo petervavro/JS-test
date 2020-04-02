6b099981792051e0321fb2bd3022c60f927e2d32
Peter <peter@vavro.me>
2020-04-02 13:39:40 -0500

    Part of solution / Task #7

--- example-7.js
+++ example-7.js
@@ -2,13 +2,124 @@ import {cleanConsole, createAll} from './data';
 
 const companies = createAll();
 
+// Part 1: Create a function taking as parameter an "id" of "company" and
+// returning the name of this "company".
+const f1 = (id) => {
+  const index = companies.findIndex((c) => (c.id === id));
+  if (index > -1) return companies[index].name;
+  return false;
+};
+
+// Part 2: Create a function taking as parameter an "id" of "company" and
+// removing the "company" from the list.
+const f2 = (id) => companies.filter((c, i) => !(c.id === id));
+
+// Part 3: Create a function taking as a parameter an "id" of "company" and
+// allowing to make a PATCH (as with an HTTP call) on all
+// attributes of this "company" except on the "users" attribute.
+/*
+const f3 = (id) => {
+  return companies;
+};
+*/
+
+// Part 4: Create a function taking as parameter an "id" of "company" and a
+// new "user" whose name is "Delgado", the first name "Juan", aged 35 and
+// a car. The new "user" must be added to the "users" list of this
+// "company" and have an automatically generated "id". The function must also modify
+// the "usersLength" attribute of "company".
+
+const f4 = (id) => {
+  return companies.map((c, i) => {
+    const users = [...c.users];
+    if (c.id === id) {
+      // The new "user" must be added to the "users" list of this
+      // "company" and have an automatically generated "id".
+      users.push({
+        firstName: 'Juan',
+        lastName: 'Delgad',
+        age: 35,
+        car: true,
+        id: users.length,
+      });
+    };
+
+    return {
+      ...c,
+      users,
+      usersLength: users.length, // Update "usersLength"
+    };
+  });
+};
+
+// Part 5: Create a function taking as parameter an "id" of "company" and
+// allowing to make a PUT (as with an HTTP call) on this "company" except
+// on the "users" attribute.
+/*
+const f5 = (id) => {
+  return companies;
+};
+*/
+
+// Part 6: Create a function taking as a parameter an "id" of "company" and a
+// "id" of "user". The function must remove this "user" from the list of "users"
+// from "company" and change the attribute "usersLength" from "company".
+const f6 = (cId, uId) => {
+  return companies.map((c) => {
+    const users = [
+      ...c.users,
+    ];
+    if (c.id === cId) {
+      const uIndex = c.users.findIndex((u) => (u.id === uId));
+
+      // Remove user
+      users.splice(uIndex, 1);
+    }
+
+    return {
+      ...c,
+      users,
+      usersLength: users.length, // Update "usersLength"
+    };
+  });
+};
+
+// Part 7: Create a function taking as a parameter an "id" of "company" and a
+// "id" of "user" allowing to make a PATCH (as with an HTTP call) on this
+// "user".
+/*
+const f7 = (cId, uId) => {
+  return companies;
+};
+*/
+
+// Part 8: Create a function taking as a parameter an "id" of "company" and a
+// "id" of "user" allowing to make a PUT (as with an HTTP call) on this
+// "user".
+/*
+const f8 = (cId, uId) => {
+  return companies;
+};
+*/
+
+// Part 9: Create a function taking as parameter two "id" of "company" and
+// an "id" of "user". The function must allow the user to be transferred as a parameter
+// from the 1st "company" to the 2nd "company". The "usersLength" attribute of each
+// "company" must be updated.
+/*
+const f9 = (cId, uId) => {
+  return companies;
+};
+*/
+
+
 cleanConsole(7, companies);
-console.log('---- EXAMPLE 7 part 1 --- ', 'Put here your function');
-console.log('---- EXAMPLE 7 part 2 --- ', 'Put here your function');
+console.log('---- EXAMPLE 7 part 1 --- ', f1(1));
+console.log('---- EXAMPLE 7 part 2 --- ', f2(1));
 console.log('---- EXAMPLE 7 part 3 --- ', 'Put here your function');
-console.log('---- EXAMPLE 7 part 4 --- ', 'Put here your function');
+console.log('---- EXAMPLE 7 part 4 --- ', f4(2));
 console.log('---- EXAMPLE 7 part 5 --- ', 'Put here your function');
-console.log('---- EXAMPLE 7 part 6 --- ', 'Put here your function');
+console.log('---- EXAMPLE 7 part 6 --- ', f6(2, 1));
 console.log('---- EXAMPLE 7 part 7 --- ', 'Put here your function');
 console.log('---- EXAMPLE 7 part 8 --- ', 'Put here your function');
 console.log('---- EXAMPLE 7 part 9 --- ', 'Put here your function');
